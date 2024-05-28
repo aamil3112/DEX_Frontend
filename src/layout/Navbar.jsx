cf7fb68ae45100d15dad66fd482b710adeff733b
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-05-28 16:44:18 +0530

    valid the git working

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,30 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="flex justify-between">
+      <div className="flex justify-between">
+        <div>POX SWAP</div>
+        <div>
+          <ul>
+            <li>Swap</li>
+            <li>Pool</li>
+            <li>Scan</li>
+            <li>LP Pools</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <ul>
+            <li>LANG</li>
+            <li>HELP</li>
+            <li>V1</li>
+          </ul>
+          </div>
+        <div>Button</div>
+      </div>
+    </div>
+    </>
   )
 }
 
