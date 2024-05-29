8a3014c1ee008bf5fa163f1429622453e6d1ce75
Shubham Gupta <gshubham804@gmail.com>
2024-05-29 09:49:19 +0530

    validation of input field done

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,12 +2,15 @@
 const InputField = ({ label, placeholder, value, onChange, children }) => {
   return (
     <div className="relative w-full">
-      <input
-        type="text"
-        className="border-1 peer block w-full appearance-none rounded-lg border border-black bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-black focus:outline-none focus:ring-0"
-        placeholder={placeholder}
-        onChange={onChange}
-      />
+     <input
+  type="number"
+  className="border-1 peer block w-full appearance-none rounded-lg border border-black bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-black focus:outline-none focus:ring-0"
+  placeholder={placeholder}
+  onChange={onChange}
+  style={{ '-moz-appearance': 'textfield' }} /* Firefox */
+  step="any" /* Chrome, Safari, Edge */
+/>
+
       <label
         className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none bg-[#E5D3F5] px-2 text-sm text-gray-500 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:px-2 peer-focus:text-black"
       >
