656ee71afa5112f6e8b11caf49cce6b8794521a2
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-05-30 14:19:04 +0530

    Beautify the animating number component

--- src/components/AnimatingNumber.jsx
+++ src/components/AnimatingNumber.jsx
@@ -1,6 +1,10 @@
 import { useSpring, animated } from '@react-spring/web';
 import PropTypes from 'prop-types';
 
+const formatNumberWithCommas = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 const AnimatedNumber = ({ value }) => {
   const { number } = useSpring({
     from: { number: 400000000 },
@@ -10,8 +14,8 @@ const AnimatedNumber = ({ value }) => {
   });
 
   return (
-    <animated.div className={"mx-3"}>
-      {number.to(n => n.toFixed(0))}
+    <animated.div className="mx-3">
+      {number.to(n => formatNumberWithCommas(n.toFixed(0)))}
     </animated.div>
   );
 };
