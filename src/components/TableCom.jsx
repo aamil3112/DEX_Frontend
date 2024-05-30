338780470940069ac6293e0491a3f91cfeb69990
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-05-30 13:30:00 +0530

    Inserted buttons in table component

--- src/components/TableCom.jsx
+++ src/components/TableCom.jsx
@@ -1,6 +1,7 @@
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { tableData } from '../data/data';
+import React from 'react';
 
 const TableCom = () => {
   const formatCurrency = (value) => {
@@ -9,6 +10,30 @@ const TableCom = () => {
 
   const priceBodyTemplate = (product) => {
     return formatCurrency(product.price);
+  };
+
+  //  // Custom template for rendering two buttons in the last column
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <div className='flex items-center py-2 space-x-4'>
+        <button onClick={() => handleEditClick(rowData)} className="bg-[#F4DBC7] text-[#CC6727] font-semibold hover:text-white p-2 rounded-lg">
+        Add Liquidity
+        </button>
+        <button onClick={() => handleDeleteClick(rowData)} className="bg-[#C65711] text-white font-bold py-2 px-4 rounded">
+        Trade
+        </button>
+      </div>
+    );
+  };
+
+  // Function to handle edit button click
+  const handleEditClick = (rowData) => {
+    alert(`Edit button clicked for ${rowData.name}`);
+  };
+
+  // Function to handle delete button click
+  const handleDeleteClick = (rowData) => {
+    alert(`Delete button clicked for ${rowData.name}`);
   };
 
   return (
@@ -64,6 +89,7 @@ const TableCom = () => {
           sortable
           style={{ width: '20%' }}
           headerClassName="yellow-header yellow-header5"
+          body={actionBodyTemplate} 
         />
       </DataTable>
     </div>
